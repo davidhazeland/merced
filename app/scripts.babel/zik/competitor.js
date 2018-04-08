998ca47d62c148f7ce967ea160fd92f14d60cf26
David Hazeland <davidhazeland@gmail.com>
2018-04-08 17:59:22 +0700

    Support product sync

--- app/scripts.babel/zik/competitor.js
+++ app/scripts.babel/zik/competitor.js
@@ -1,28 +1,28 @@
-// const compareDate = (a, b) => {
-//   const timeDiff = Math.abs(b.getTime() - a.getTime());
-//   return Math.ceil(timeDiff / (1000 * 3600 * 24));
-// };
-//
-// const ci = (sale = 10, price = 10, date = 365) => {
-//   const data = $('#datatable-responsive').dataTable().fnGetData();
-//   const filtered = data.filter(row => {
-//     const validSale = parseFloat(row[3]) >= sale;
-//     const validPrice = parseFloat(row[5].split(' ')[1]) >= price;
-//     const validDate = compareDate(new Date(), new Date(row[2])) <= date;
-//     return validSale && validPrice && validDate;
-//   });
-//   if (filtered.length === 0) return;
-//   const idList = filtered.map(row => {
-//     const id = row[1].match(/.+ebay.com\/itm\/(\d+)".+/)[1];
-//     return {
-//       id,
-//       date: row[2],
-//       sold: row[3],
-//       price: row[5]
-//     }
-//   });
-//   return _.keyBy(idList, 'id');
-// };
+const compareDate = (a, b) => {
+  const timeDiff = Math.abs(b.getTime() - a.getTime());
+  return Math.ceil(timeDiff / (1000 * 3600 * 24));
+};
+
+const ci = (sale = 10, price = 5, date = 365) => {
+  const data = $('#datatable-responsive').dataTable().fnGetData();
+  const filtered = data.filter(row => {
+    const validSale = parseFloat(row[3]) >= sale;
+    const validPrice = parseFloat(row[5].split(' ')[1]) >= price;
+    const validDate = compareDate(new Date(), new Date(row[2])) <= date;
+    return validSale && validPrice && validDate;
+  });
+  if (filtered.length === 0) return;
+  const idList = filtered.map(row => {
+    const id = row[1].match(/.+ebay.com\/itm\/(\d+)".+/)[1];
+    return {
+      id,
+      date: row[2],
+      sold: row[3],
+      price: row[5]
+    }
+  });
+  return idList;
+};
 
 function getUrlVars()
 {
@@ -57,37 +57,46 @@ database.ref('/seller-list').once('value').then(snapshot => {
     Competitor
   ]));
 
-  console.log('synced');
+  console.log('Seller synced');
 });
 
-// $(document).ready(function () {
-//   const config = {
-//     apiKey: "AIzaSyCh2JzXMnYajBzt7ytjHVe_Q6sXNFLb6YE",
-//     authDomain: "merced-13.firebaseapp.com",
-//     databaseURL: "https://merced-13.firebaseio.com",
-//     projectId: "merced-13",
-//     storageBucket: "merced-13.appspot.com",
-//     messagingSenderId: "1094482201047"
-//   };
-//   firebase.initializeApp(config);
-//
-//   setTimeout(() => {
-//     const database = firebase.database();
-//     const username = $('#srcUsername').val();
-//
-//     const idList = ci();
-//
-//     if (!username) {
-//       return;
-//     }
-//     if (idList) {
-//       database.ref('/sellers/' + username).set(idList);
-//       console.log('synced');
-//     } else {
-//       database.ref('/sellers/' + username).set({
-//         isSynced: true
-//       });
-//       console.log('nothing to sync');
-//     }
-//   }, 5000)
-// });
+$(document).ready(function () {
+  setTimeout(function() {
+    const itemList = ci();
+    if (!itemList) {
+      console.log('nothing to sync');
+      return;
+    }
+    const tasks = itemList.map(item => {
+      return (callback) => {
+        // Default options are marked with *
+        return fetch('https://seed.dropist.io/api/items', {
+          body: JSON.stringify({
+            item_id: item.id
+          }),
+          headers: {
+            'accept': 'application/json',
+            'content-type': 'application/json',
+          },
+          method: 'POST'
+        })
+          .then(response => {
+            return response.json();
+          })
+          .catch(err => {
+            callback(err);
+          })
+          .then(result => {
+            callback(null, result)
+          })
+      }
+    });
+    async.parallelLimit(tasks, 20, (err, results) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(results);
+      alert('Products synced.')
+    })
+  }, 5000)
+});
